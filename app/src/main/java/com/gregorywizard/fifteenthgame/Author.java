ac01ecb509800d6155970d6d7c6f4b9d49d77596
Gregory Wizard <gregoryurf@yandex.ru>
2016-03-28 15:21:15 +0300

    Link to github.

--- app/src/main/java/com/gregorywizard/fifteenthgame/Author.java
+++ app/src/main/java/com/gregorywizard/fifteenthgame/Author.java
@@ -102,7 +102,7 @@ public class Author extends AppCompatActivity {
     private class buttonDefaultListener implements View.OnClickListener {
 
         public void onClick(View _v) {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/GregoryWizard/15"));
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/GregoryWizard/FifteenthGame"));
             startActivity(browserIntent);
 
         }
